0bc511d2125752d5e630b4132865650e44a28ad7
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-02 10:31:24 +0100

    setup test for day2 part 1

---
+++ adventofcode.tests/test/day2tests.cpp
@@ -0,0 +1,40 @@
+#include "pch.h"
+
+#include <fstream>
+
+#include "Day2.h"
+
+class day2Fixture : public ::testing::Test {
+
+private:
+	std::vector<std::string> _tokens;
+
+public:
+	void SetUp(std::string path) {
+		std::vector<std::string> tokens;
+		std::string line;
+		std::ifstream stream;
+		stream.open(path, std::iostream::in);
+		while (std::getline(stream, line))
+		{
+			tokens.push_back(line);
+		}
+
+		if (stream.is_open())
+		{
+			stream.close();
+		}
+
+		_tokens = tokens;
+	}
+
+	std::vector<std::string> getTokens() {
+		return _tokens;
+	}
+};
+
+TEST_F(day2Fixture, Example1) {
+	SetUp("day2_fixture_1.txt");
+	auto actual = Day2::Part1(getTokens());
+	EXPECT_EQ(12, actual);
+}
---
+++ adventofcode/src/Day2.cpp
@@ -0,0 +1,8 @@
+#include "Day2.h"
+
+namespace Day2 {
+
+	int Part1(std::vector<std::string> tokens) {
+		return 0;
+	}
+}
