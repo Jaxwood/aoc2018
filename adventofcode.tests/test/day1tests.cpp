cb535ba0de8db05a8ce6356ff29292f8626bd789
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-02 07:36:09 +0100

    move examples for day1.1

--- adventofcode.tests/test/day1tests.cpp
+++ adventofcode.tests/test/day1tests.cpp
@@ -3,7 +3,17 @@
 #include "Day1.h"
 
 
-TEST(Day1, Solution1) {
-	auto actual = Day1::Sum("day1_fixture.txt");
+TEST(Day1, Example1) {
+	auto actual = Day1::Sum("day1_fixture_1.txt");
 	EXPECT_EQ(0, actual);
+}
+
+TEST(Day1, Example2) {
+	auto actual = Day1::Sum("day1_fixture_2.txt");
+	EXPECT_EQ(3, actual);
+}
+
+TEST(Day1, Example3) {
+	auto actual = Day1::Sum("day1_fixture_3.txt");
+	EXPECT_EQ(-6, actual);
 }
