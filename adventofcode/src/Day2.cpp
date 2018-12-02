cba8da6121e9aa819abf3514417fa1bbc04d01f8
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-02 10:50:07 +0100

    solution for part 1 of day2

--- adventofcode/src/Day2.cpp
+++ adventofcode/src/Day2.cpp
@@ -1,8 +1,31 @@
 #include "Day2.h"
 
+#include <algorithm>
+
 namespace Day2 {
 
 	int Part1(std::vector<std::string> tokens) {
-		return 0;
+		int twos = 0;
+		int threes = 0;
+		bool two = false;
+		bool three = false;
+		for (auto i = 0; i < tokens.size(); i++) {
+			auto candidate = tokens[i];
+			two = false;
+			three = false;
+			for (auto j = 0; j < candidate.size(); j++) {
+				auto cnt = std::count(std::begin(candidate), std::end(candidate), candidate[j]);
+				if (!two && cnt == 2) {
+					twos = twos + 1;
+					two = true;
+				}
+				if (!three && cnt == 3) {
+					threes = threes + 1;
+					three = true;
+				}
+			}
+		}
+
+		return twos * threes;
 	}
 }
