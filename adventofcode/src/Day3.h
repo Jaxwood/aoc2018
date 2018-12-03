61104df7fedc30c5086e074ecafe460b8a695b02
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-03 20:56:59 +0100

    introduce class to store fabrics

--- adventofcode/src/Day3.h
+++ adventofcode/src/Day3.h
@@ -7,33 +7,33 @@ namespace Day3 {
 	class Fabric {
 
 	private:
-		int _x;
-		int _y;
-		int _w;
-		int _h;
+		int _height;
+		int _width;
+		int _left;
+		int _top;
 
 	public:
-		Fabric(int w, int h, int x, int y) {
-			_x = x;
-			_y = y;
-			_h = h;
-			_w = w;
+		Fabric(int left, int top, int width, int height) {
+			_height = height;
+			_width = width;
+			_top = top;
+			_left = left;
 		};
 
 		int getWidth() {
-			return _w;
+			return _width;
 		}
 
 		int getHeight() {
-			return _h;
+			return _height;
 		}
 
-		int getX() {
-			return _x;
+		int getTop() {
+			return _top;
 		}
 
-		int getY() {
-			return _y;
+		int getLeft() {
+			return _left;
 		}
 	};
 
