c4feb4a69e951055bfed873fa9a797da50d18ae8
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-05 20:27:54 +0100

    solution for day5 part 1

--- adventofcode/src/Day5.cpp
+++ adventofcode/src/Day5.cpp
@@ -1,8 +1,29 @@
 #include "Day5.h"
 
+#include <cctype>
+#include <string>
+
 namespace Day5 {
 
 	int Part1(std::string input) {
-		return 0;
+
+		for (int i = 0; i < input.size(); i++) {
+			if (std::isupper(input[i]))
+			{
+				if (input[i+1] == std::tolower(input[i]))
+				{
+					return Part1(input.erase(i, 2));
+				}
+			}
+			else
+			{
+				if (input[i+1] == std::toupper(input[i]))
+				{
+					return Part1(input.erase(i, 2));
+				}
+			}
+		}
+
+		return input.size();
 	}
 }
