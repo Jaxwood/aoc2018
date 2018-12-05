41006f0beef7939efbfeb495aeb0cedd102ef699
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-05 19:50:17 +0100

    solutoin for part1

--- adventofcode/src/Day4.cpp
+++ adventofcode/src/Day4.cpp
@@ -1,11 +1,70 @@
 #include "Day4.h"
 
 #include <algorithm>
+#include <numeric>
+#include <map>
+#include <tuple>
 
 namespace Day4 {
  
 	int Part1(std::vector<Record> tokens) {
 		std::sort(std::begin(tokens), std::end(tokens));
-		return 0;
+		std::map<int, std::vector<std::tuple<int, int>>> sleepingSchedule;
+		int guard = 0;
+		int start, end;
+		for (int i = 0; i < tokens.size(); i++) {
+			auto inst = tokens[i];
+			if (inst.isGuard()) {
+				guard = inst.id();
+				continue;
+			}
+			if (inst.sleeps()) {
+				sleepingSchedule[guard].push_back(std::make_tuple(inst.getTime(), 0));
+			}
+			if (inst.awake()) {
+				std::tie (start, end) = sleepingSchedule[guard].back();
+				sleepingSchedule[guard].pop_back();
+				sleepingSchedule[guard].push_back(std::make_tuple(start, inst.getTime()));
+			}
+		}
+
+		// who sleeps the most?
+		int max = 0;
+		guard = 0;
+		for (auto const &it : sleepingSchedule)
+		{
+			auto v = sleepingSchedule[it.first];
+			auto candidate = std::accumulate(v.begin(), v.end(), 0, [](int acc, std::tuple<int, int> next) {
+				int s, e;
+				std::tie (s, e) = next;
+				acc += (e - s);
+				return acc;
+			});
+			if (candidate > max)
+			{
+				guard = it.first;
+				max = candidate;
+			}
+		}
+
+		// which minute is the candidate most likely to sleep?
+		auto candidate = sleepingSchedule[guard];
+		std::map<int, int> minutes;
+		for (auto const &it : candidate) {
+			std::tie (start, end) = it;
+			for (int i = start; i < end; i++) {
+				if (minutes.count(i)) {
+					minutes[i]++;
+				}
+				else {
+					minutes[i] = 1;
+				}
+			}
+		}
+		std::pair<int, int> maximum = *(std::max_element(std::begin(minutes), std::end(minutes), [](std::pair<int, int> p, std::pair<int, int> a) {
+			return p.second < a.second;
+		}));
+
+		return guard * maximum.first;
 	}
 }
