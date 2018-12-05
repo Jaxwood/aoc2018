c080146cffd6f8dbd029f26774b10479504e34ee
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-05 21:28:03 +0100

    optimize solution for input

--- adventofcode/src/Day5.cpp
+++ adventofcode/src/Day5.cpp
@@ -7,19 +7,31 @@ namespace Day5 {
 
 	int Part1(std::string input) {
 
-		for (int i = 0; i < input.size(); i++) {
+		int i = 0;
+		while (i < input.size())
+		{
 			if (std::isupper(input[i]))
 			{
-				if (input[i+1] == std::tolower(input[i]))
+				if (input[i + 1] == std::tolower(input[i]))
 				{
-					return Part1(input.erase(i, 2));
+					input = input.erase(i, 2);
+					i = 0;
+				}
+				else
+				{
+					i++;
 				}
 			}
 			else
 			{
-				if (input[i+1] == std::toupper(input[i]))
+				if (input[i + 1] == std::toupper(input[i]))
 				{
-					return Part1(input.erase(i, 2));
+					input = input.erase(i, 2);
+					i = 0;
+				}
+				else
+				{
+					i++;
 				}
 			}
 		}
