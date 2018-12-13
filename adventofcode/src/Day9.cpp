7855359925c45d5819a453959bac61e820398b01
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-13 21:58:10 +0100

    refactor for clarity

--- adventofcode/src/Day9.cpp
+++ adventofcode/src/Day9.cpp
@@ -2,65 +2,76 @@
 
 #include <map>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 namespace Day9 {
 
-	int scorePosition(int position, int size) {
-		if (position - 7 < 0) {
-			return size - position - 7;
+	int getNextPosition(int position, int size)
+	{
+		// are we at the end?
+		if (position == size - 1) {
+			position = 1;
 		}
 		else {
-			return position - 7;
+			position += 2;
 		}
+		return position;
+	}
+
+	int getScoringPosition(int position, int size) {
+		if (position > 7) {
+			position -= 7;
+		}
+		else {
+			int diff = 7 - position;
+			position = size - diff;
+		}
+		return position;
 	}
 
 	int Part1(int participants, int lastMable) {
 		// setup game
-		map<int, int> score;
-		size_t player = 1;
+		map<int, int> players;
+		vector<int> score;
+		score.push_back(0);
+		score.push_back(1);
 		int position = 0;
-		vector<int> mables;
-		mables.push_back(0);
+		int playerTurn = 2;
 
-		for (size_t i = 1; i < lastMable; i++) {
-			auto size = mables.size();
-			// update score
+		// turns
+		for (int i = 2; i <= lastMable; i++) {
+			auto size = score.size();
+
+			// is it a scoring turn?
 			if (i % 23 == 0) {
-				position = scorePosition(position, size);
-				score[player] = i;
-				score[player] += mables.at(position);
-				mables.erase(begin(mables) + position);
-			}
-
-			// update position
-			mables.push_back(i);
-
-			if (position + 2 >= size) {
-				position = 1;
+				position = getScoringPosition(position, size);
+				players[playerTurn] += i;
+				players[playerTurn] += score.at(position);
+				score.erase(begin(score) + position);
 			}
 			else {
-				position += 2;
+				position = getNextPosition(position, size);
+				score.insert(begin(score) + position, 1, i);
 			}
 
-			// next turn
-			if (player == participants) {
-				player = 1;
+			// update active player
+			if (playerTurn == participants) {
+				playerTurn = 1;
 			}
 			else {
-				player++;
+				++playerTurn;
+			}
+		}
+		int highscore = 0;
+		for (auto &p : players) {
+			if (p.second > highscore) {
+				highscore = p.second;
 			}
 		}
 
-		// get the highest score
-		int total = 0;
-		for (auto &p : score) {
-			if (p.second > total) {
-				total = p.second;
-			}
-		}
-		return total;
+		return highscore;
 	}
 
 	int Part2(int players, int lastMable) {
