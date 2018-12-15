68234feae4a19af5d28bdb35a2bccfc6f23ae5a6
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-15 10:03:38 +0100

    solution for day11 Part 2

--- adventofcode/src/Day11.cpp
+++ adventofcode/src/Day11.cpp
@@ -1,76 +1,42 @@
 #include "Day11.h"
 
-#include <numeric>
-#include <vector>
-#include <tuple>
-#include <map>
-
 using namespace std;
 
 namespace Day11 {
 
-	int hundredDigit(int num) {
-		vector<int> digits;
-		while (num >= 10) {
-			digits.push_back(num % 10);
-			num = num / 10;
-		}
-		digits.push_back(num);
-		if (digits.size() < 3) {
-			num = 0;
-		}
-		else {
-			num = digits[2];
-		}
-		return num;
-	}
-
-	int calculatePowerLevel(int x, int y, int serialNumber) {
-		auto rackId = x + 10;
-		int powerlevel = (rackId * y) + serialNumber;
-		powerlevel = powerlevel * rackId; 
-		return hundredDigit(powerlevel) - 5;
-	}
-
-	vector<tuple<int, int>> getCoords(int x, int y, int sizeX, int sizeY) {
-		vector<tuple<int, int>> result;
-		for (int i = 0; i < sizeX; i++) {
-			for (int j = 0; j < sizeY; j++) {
-				result.push_back(make_tuple(x+i, y+j));
-			}
-		}
-		return result;
-	}
-
-	int calculateTotalPowers(vector<tuple<int, int>> coords, int serialNumber) {
-		int x, y;
-		int total = 0;
-		for (int i = 0; i < coords.size(); i++) {
-			tie(x, y) = coords[i];
-			total += calculatePowerLevel(x, y, serialNumber);
-		}
-		return total;
-	}
-
-	tuple<int, int> Part1(int sizeX, int sizeY, int serialNumber) {
-		map<tuple<int, int>, int> totals;
-		// loop x
-		for (int i = 1; i <= 300 - sizeX; i++) {
-			// loop y
-			for (int j = 1; j <= 300 - sizeY; j++) {
-				auto coords = getCoords(i, j, sizeX, sizeY);
-				totals[make_tuple(i,j)] = calculateTotalPowers(coords, serialNumber);
-			}
-		}
+	tuple<int, int> Part1(int size, int serialNumber) {
 		int best = 0;
+		auto grid = Grid(serialNumber);
+		
 		tuple<int, int> candidate;
-		for (auto &t : totals) {
-			if (t.second > best) {
-				best = t.second;
-				candidate = t.first;
+		for (int x = 1; x <= 300 - size; x++) {
+			for (int y = 1; y <= 300 - size; y++) {
+				auto result = grid.totals(x, y, size);
+				if (result > best) {
+					best = result;
+					candidate = make_tuple(x, y);
+				}
 			}
 		}
 		return candidate;
 	}
 
+	tuple<int, int, int> Part2(int serialNumber) {
+		tuple<int, int, int> candidate;
+		int best = 0;
+		auto grid = Grid(serialNumber);
+		
+		for (int size = 1; size <= 300; size++) {
+			for (int x = 1; x <= 300 - size; x++) {
+				for (int y = 1; y <= 300 - size; y++) {
+					auto result = grid.totals(x, y, size);
+					if (result > best) {
+						best = result;
+						candidate = make_tuple(x, y, size);
+					}
+				}
+			}
+		}
+		return candidate;
+	}
 }
--- adventofcode/src/Day11.h
+++ adventofcode/src/Day11.h
@@ -1,5 +1,64 @@
 #pragma once
+
+#include <numeric>
+#include <vector>
 #include <tuple>
+#include <map>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
 namespace Day11 {
-	std::tuple<int, int> Part1(int sizeX, int sizeY, int serialNumber);
+	class Grid {
+	private:
+		int serialNumber;
+
+		int hundredDigit(int num) {
+			vector<int> digits;
+			while (num >= 10) {
+				digits.push_back(num % 10);
+				num = num / 10;
+			}
+			digits.push_back(num);
+			if (digits.size() < 3) {
+				num = 0;
+			}
+			else {
+				num = digits[2];
+			}
+			return num;
+		}
+
+		int calculatePowerLevel(int x, int y) {
+			auto rackId = x + 10;
+			int powerlevel = (rackId * y) + this->serialNumber;
+			powerlevel = powerlevel * rackId;
+			auto result = hundredDigit(powerlevel) - 5;
+			return result;
+		}
+
+		int getCoords(int x, int y, int size) {
+			int result = 0;
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < size; j++) {
+					result += calculatePowerLevel(x + i, y + j);
+				}
+			}
+			return result;
+		}
+
+	public:
+		Grid(int serialNumber) {
+			this->serialNumber = serialNumber;
+		}
+
+		int totals(int x, int y, int size) {
+			return this->getCoords(x, y, size);
+		}
+	};
+
+	std::tuple<int, int> Part1(int size, int serialNumber);
+
+	std::tuple<int, int, int> Part2(int serialNumber);
 }
