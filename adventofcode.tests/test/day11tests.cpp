e8d96824cfcc39c8a08d7e68cec6064a8fec6aba
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-15 10:03:38 +0100

    solution for day11 Part 2

--- adventofcode.tests/test/day11tests.cpp
+++ adventofcode.tests/test/day11tests.cpp
@@ -3,7 +3,11 @@
 #include "Day11.h"
 #include <tuple>
 
-TEST(Day11, Part1a) {
-	auto actual = Day11::Part1(3, 3, 5034);
+TEST(Day11, Part1) {
+	auto actual = Day11::Part1(3, 5034);
 	EXPECT_EQ(std::make_tuple(235,63), actual);
+}
+TEST(Day11, Part2) {
+	auto actual = Day11::Part2(5034);
+	EXPECT_EQ(std::make_tuple(235,63, 0), actual);
 }
