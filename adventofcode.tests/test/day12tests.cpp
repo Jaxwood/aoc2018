73f6e57b65f1dd39e567ce82a8cc1e6becc6e5c4
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-16 08:06:26 +0100

    Setup for day12

---
+++ adventofcode.tests/test/day12tests.cpp
@@ -0,0 +1,61 @@
+#include "pch.h"
+
+#include <fstream>
+#include <algorithm>
+#include <regex>
+#include <tuple>
+#include <iostream>
+#include <map>
+
+#include "Day12.h"
+
+using namespace std;
+using namespace Day12;
+
+class day12Fixture : public ::testing::Test {
+
+private:
+	std::vector<std::string> _tokens;
+
+public:
+	void SetUp(std::string path) {
+		std::vector<std::string> tokens;
+		std::string line;
+		std::ifstream stream;
+		stream.open(path, std::iostream::in);
+		while (std::getline(stream, line))
+		{
+			tokens.push_back(line);
+		}
+
+		if (stream.is_open())
+		{
+			stream.close();
+		}
+
+		_tokens = tokens;
+	}
+
+	map<string, string> getTokens() {
+		map<string, string> records;
+		auto reg = regex("([.|#]+)\\s=>\\s([#|.])", regex_constants::ECMAScript);
+		for (int i = 0; i < this->_tokens.size(); i++) {
+			smatch match;
+			if (regex_match(this->_tokens[i], match, reg)) {
+				records[match[1]] = match[2];
+			}
+		}
+		return records;
+	}
+};
+
+TEST_F(day12Fixture, Part1) {
+	SetUp("day12_fixture.txt");
+	auto actual = Day12::Part1("#..#.#..##......###...###", getTokens());
+	EXPECT_EQ(325, actual);
+}
+TEST_F(day12Fixture, Part1a) {
+	SetUp("day12.txt");
+	auto actual = Day12::Part1("#........#.#.#...###..###..###.#..#....###.###.#.#...####..##..##.#####..##...#.#.....#...###.#.####", getTokens());
+	EXPECT_EQ(325, actual);
+}
---
+++ adventofcode/src/Day12.cpp
@@ -0,0 +1,42 @@
+#include "Day12.h"
+
+
+namespace Day12 {
+
+	int Part1(string initialState, map<string, string> input) {
+		initialState = ".." + initialState + "..";
+		int sum = 0;
+		int idx = 2;
+		for (int g = 0; g < 20; g++) {
+			string generation = "";
+			for (int i = 2; i < initialState.size() - 2; i++) {
+				string pattern = "";
+				copy_n(begin(initialState) + i - 2, 5, back_inserter(pattern));
+				if (input.find(pattern) != input.end()) {
+					generation += input[pattern];
+				}
+				else {
+					generation += '.';
+				}
+			}
+			initialState = "..." + generation + "...";
+			idx += 1;
+		}
+		string positives;
+		string negatives;
+		copy(begin(initialState) + idx, end(initialState), back_inserter(positives));
+		copy(begin(initialState), begin(initialState) + idx + 1, back_inserter(negatives));
+		for (int i = 0; i < positives.size(); i++) {
+			if (positives[i] == '#') {
+				sum += i;
+			}
+		}
+		reverse(begin(negatives), end(negatives));
+		for (int i = 0; i <= negatives.size(); i++) {
+			if (negatives[i] == '#') {
+				sum -= i;
+			}
+		}
+		return sum;
+	}
+}
