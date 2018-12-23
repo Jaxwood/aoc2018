c4d5bcd8d2c15731ad0809c88129d82826dc2433
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-23 14:44:54 +0100

    final solution for part2 of day12

--- adventofcode/src/Day12.cpp
+++ adventofcode/src/Day12.cpp
@@ -2,12 +2,13 @@
 
 namespace Day12 {
 	string currentState;
+	int startIdx;
 
 	int sum(int generations, string state) {
 		int sum = 0;
 		for (int i = 0; i < state.size(); i++) {
 			if (state[i] == '#') {
-				sum += i - generations;
+				sum += generations + i;
 			}
 		}
 		return sum;
@@ -17,30 +18,38 @@ namespace Day12 {
 		return currentState;
 	}
 
+	int getStartIdx() {
+		return startIdx;
+	}
+
 	string getPattern(int idx, string state) {
-		if (idx == 1) {
-			return "." + state.substr(idx - 1, 4);
+		if (idx < 2) {
+			return string(2 - idx, '.') + state.substr(0, 3 + idx);
 		}
-		if (idx == 0) {
-			return ".." + state.substr(idx, 3);
-		}
-		if (idx == (-1)) {
-			return "..." + state.substr(0, 2);
-		}
-		if (idx == state.size() - 2) {
-			return state.substr(idx - 2, 4) + ".";
-		}
-		if (idx == state.size() - 1) {
-			return state.substr(idx - 2, 3) + "..";
-		}
-		if (idx == state.size()) {
-			return state.substr(idx - 2, 2) + "...";
+		auto size = state.size();
+		if (idx > size - 3) {
+			return state.substr(idx - 2, size - idx + 2) + string(3 - (size - idx), '.');
 		}
 		return state.substr(idx-2, 5);
 	}
 
+	string& rtrim(string& str)
+	{
+		str.erase(str.find_last_not_of('.') + 1);
+		return str;
+	}
+
+	string& ltrim(string& str)
+	{
+		int cnt = str.find_first_not_of('.');
+		startIdx += cnt;
+		str.erase(0, cnt);
+		return str;
+	}
+
 	int Part1(int generations, string state, unordered_map<string, string> rules) {
 		// for each generation
+		startIdx = 0;
 		for (int i = 0; i < generations; i++) {
 			string nextState = "";
 			// generate next generation
@@ -51,12 +60,21 @@ namespace Day12 {
 					// match rule
 					value = rules[pattern];
 				}
-				nextState += value;
+				if (j == 0 && value == ".") {
+					// do nothing
+				}
+				else {
+					if (j == 0) {
+						startIdx--;
+					}
+					nextState += value;
+				}
 			}
-			currentState = nextState;
+
+			currentState = ltrim(rtrim(nextState));
 			state = nextState;
 		}
 
-		return sum(generations, state);
+		return sum(startIdx, state);
 	}
 }
