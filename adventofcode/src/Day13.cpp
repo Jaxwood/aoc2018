5515862b22a6ba5a81912a12ef5ef7d211d78979
Jaxwood <lorenzen.jacob@gmail.com>
2018-12-24 16:19:22 +0100

    Introduce Network class

--- adventofcode/src/Day13.cpp
+++ adventofcode/src/Day13.cpp
@@ -2,56 +2,11 @@
 
 namespace Day13 {
 
-	bool isCart(char candidate) {
-		return candidate == '>' || candidate == '<' || candidate == 'v' || candidate == '^';
-	}
-
-	Direction toDirection(char candidate) {
-		switch (candidate) {
-		case '>':
-			return East;
-		case '<':
-			return West;
-		case 'v':
-			return South;
-		case '^':
-			return North;
-		default:
-			throw exception("unknown direction");
-		}
-	}
-
-	char replaceDirection(Direction direction) {
-		switch (direction) {
-		case South:
-		case North:
-			return '|';
-		case East:
-		case West:
-			return '-';
-		default:
-			throw exception("unknown direction");
-		}
-	}
 
 	tuple<int, int> Part1(vector<string> lines) {
-		char grid[6][13];
-		vector<Cart> carts;
-		auto s1 = lines.size();
-		auto s2 = lines[0].size();
-		for (int i = 0; i < lines.size(); i++) {
-			for (int j = 0; j < lines[i].size(); j++) {
-				auto candidate = lines[i][j];
-				if (isCart(candidate)) {
-					auto direction = toDirection(candidate);
-					carts.push_back(Cart(i, j, direction));
-					grid[i][j] = replaceDirection(direction);
-				}
-				else {
-					grid[i][j] = candidate;
-				}
-			}
-		}
-		return make_tuple(0, 0);
+		auto network = Network(lines);
+		network.setup();
+		while (network.tick()) {}
+		return network.collesion();
 	}
 }
--- adventofcode/src/Day13.h
+++ adventofcode/src/Day13.h
@@ -26,6 +26,76 @@ namespace Day13 {
 		}
 	};
 
+	class Network {
+	private:
+		vector<string> lines;
+		vector<Cart> carts;
+		char grid[6][13];
+
+		bool isCart(char candidate) {
+			return candidate == '>' || candidate == '<' || candidate == 'v' || candidate == '^';
+		}
+
+		Direction toDirection(char candidate) {
+			switch (candidate) {
+			case '>':
+				return East;
+			case '<':
+				return West;
+			case 'v':
+				return South;
+			case '^':
+				return North;
+			default:
+				throw exception("unknown direction");
+			}
+		}
+
+		char replaceDirection(Direction direction) {
+			switch (direction) {
+			case South:
+			case North:
+				return '|';
+			case East:
+			case West:
+				return '-';
+			default:
+				throw exception("unknown direction");
+			}
+		}
+
+	public:
+		Network(vector<string> lines) {
+			this->lines = lines;
+		}
+
+		void setup() {
+			vector<Cart> carts;
+			for (int i = 0; i < lines.size(); i++) {
+				for (int j = 0; j < lines[i].size(); j++) {
+					auto candidate = lines[i][j];
+					if (this->isCart(candidate)) {
+						auto direction = this->toDirection(candidate);
+						carts.push_back(Cart(i, j, direction));
+						this->grid[i][j] = this->replaceDirection(direction);
+					}
+					else {
+						this->grid[i][j] = candidate;
+					}
+				}
+			}
+			this->carts = carts;
+		}
+
+		bool tick() {
+			return false;
+		}
+
+		tuple<int, int> collesion() {
+			return make_tuple(0, 0);
+		}
+	};
+
 	tuple<int, int> Part1(vector<string> lines);
 
 }
