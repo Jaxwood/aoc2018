f8f95fde93f02147952211375b3a56ae01d9c4c1
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-26 09:29:41 +0100

    Solution for day14 part 1

--- adventofcode/src/Day14.cpp
+++ adventofcode/src/Day14.cpp
@@ -1,7 +1,65 @@
 #include "Day14.h"
 
+using namespace std;
+
 namespace Day14 {
-	string Part1(int recipes) {
-		return "0";
+
+	Elv::Elv(int idx, int offset) {
+		this->idx = idx;
+		this->offset = offset;
+	}
+
+	int Elv::getIndex() {
+		return this->idx;
+	}
+
+	int Elv::getOffset() {
+		return this->offset;
+	}
+
+	void Elv::setNextIndex(int index) {
+		this->idx = index;
+	}
+
+	void Elv::updateOffset(int size) {
+		auto total = this->offset + this->idx;
+		this->offset = total % size;
+	}
+
+	Reciepe::Reciepe(string initial) {
+		this->reciepes = initial;
+	}
+
+	vector<Elv> Reciepe::produce(vector<Elv> elves) {
+		int sum = 0;
+		for (auto &elv : elves) {
+			auto idx = elv.getOffset();
+			auto num = this->reciepes[idx] - '0';
+			sum += num;
+			elv.setNextIndex(num + 1);
+		}
+		this->reciepes += to_string(sum);
+		int size = this->reciepes.size();
+		for (auto &elv : elves) {
+			elv.updateOffset(size);
+		}
+		return elves;
+	}
+
+	int Reciepe::size() {
+		return this->reciepes.size();
+	}
+
+	string Reciepe::result(int count) {
+		return this->reciepes.substr(count, 10);
+	}
+
+	string Part1(int count) {
+		vector<Elv> elves = { Elv(0, 0), Elv(1, 1) };
+		auto reciepe = Reciepe("37");
+		while(reciepe.size() < count + 10) {
+			elves = reciepe.produce(elves);
+		}
+		return reciepe.result(count);
 	}
 }
--- adventofcode/src/Day14.h
+++ adventofcode/src/Day14.h
@@ -1,9 +1,30 @@
 #pragma once
 
 #include <string>
-
-using namespace std;
+#include <vector>
 
 namespace Day14 {
-	string Part1(int recipes);
+	class Elv {
+	private:
+		int idx;
+		int offset;
+	public:
+		Elv(int index, int offset);
+		void setNextIndex(int index);
+		void updateOffset(int size);
+		int getIndex();
+		int getOffset();
+	};
+
+	class Reciepe {
+	private:
+		std::string reciepes;
+	public:
+		Reciepe(std::string initial);
+		std::vector<Elv> produce(std::vector<Elv> elves);
+		int size();
+		std::string result(int count);
+	};
+
+	std::string Part1(int recipes);
 }
