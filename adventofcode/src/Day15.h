f9c8a00226fcdbb73ba549a11ffd9c3e21a497be
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-27 07:50:13 +0100

    Introduce Player and Game types

--- adventofcode/src/Day15.h
+++ adventofcode/src/Day15.h
@@ -1,36 +1,39 @@
 #pragma once
 
+#include <algorithm>
 #include <string>
 #include <vector>
 
 namespace Day15 {
 
-	class Creature {
+	enum PlayerType {
+		Elv,
+		Goblin
+	};
+
+	class Player {
 	protected:
-		int x, y;
+		int x, y, hitpoints;
+		PlayerType type;
 	public:
-		Creature(int x, int y);
+		Player(int x, int y, char type);
+		void attack(Player &creature);
+		void damage();
+		bool operator<(Player &other);
 	};
 
-	class Elv : public Creature {
-	public:
-		Elv(int x, int y);
-	};
-
-	class Goblin : public Creature {
-	public:
-		Goblin(int x, int y);
-	};
-
-	class Dungeon {
+	class Game {
 	private:
 		char grid[7][7];
 		std::vector<std::string> lines;
-		std::vector<Elv> elves;
-		std::vector<Goblin> goblins;
+		std::vector<Player> players;
+		int turnCount;
 	public:
-		Dungeon(std::vector<std::string> lines);
+		Game(std::vector<std::string> lines);
 		void setup();
+		void turn();
+		bool over();
+		int turns() { return this->turnCount; }
 	};
 
 	int Part1(std::vector<std::string> lines);
