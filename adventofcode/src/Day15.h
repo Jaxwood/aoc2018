ae84d3f5a31df8b7d353101832a503b47ac023e2
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-12-30 17:39:00 +0100

    Introduce atlas

--- adventofcode/src/Day15.h
+++ adventofcode/src/Day15.h
@@ -8,6 +8,26 @@
 #include <vector>
 
 namespace Day15 {
+
+	typedef std::tuple<int, int> Point;
+
+	class Atlas {
+		const int size = 10;
+		const char OPEN = '.';
+		char grid[10][10];
+	public:
+		void initialize(std::vector<std::string> raw);
+		bool equal(const Atlas &other) const;
+		void swap(Point p1, Point p2);
+		void clear(Point point);
+		std::vector<Point> neighbors(Point point) const;
+		std::vector<Point> types(const char c) const;
+	};
+
+	bool operator==(const Atlas &a1, const Atlas &a2);
+
+	class PathFinder {
+	};
+
 	int Part1(std::vector<std::string> lines);
-	void Setup(std::vector<std::string> &lines);
 }
