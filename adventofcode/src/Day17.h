e189b93762a0851a2221022a64341355b6362c5d
Jacob Lorenzen <jl@danfoss.com>
2019-01-02 15:35:47 +0100

    Fill water

--- adventofcode/src/Day17.h
+++ adventofcode/src/Day17.h
@@ -1,14 +1,21 @@
 #pragma once
 
+#include <map>
 #include <string>
 #include <vector>
 
 namespace Day17 {
 	class Clay {
-		std::vector<int> xs;
-		std::vector<int> ys;
+		std::vector<int> _xs;
+		std::vector<int> _ys;
 	public:
-		Clay(std::vector<int> xs, std::vector<int> ys) : xs(xs), ys(ys) {};
+		Clay(std::vector<int> xs, std::vector<int> ys) : _xs(xs), _ys(ys) {};
+		std::vector<int> ys() {
+			return this->_ys;
+		}
+		std::vector<int> xs() {
+			return this->_xs;
+		}
 	};
 	int Part1(std::vector<Clay> lines);
 }
