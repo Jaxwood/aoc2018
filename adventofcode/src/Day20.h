b73fb10dc06086fc26819741506e5fa9789a2c97
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-01-13 20:05:15 +0100

    WIP

--- adventofcode/src/Day20.h
+++ adventofcode/src/Day20.h
@@ -2,13 +2,37 @@
 
 #include <algorithm>
 #include <map>
+#include <set>
 #include <string>
 #include <tuple>
 #include <vector>
 
 namespace Day20 {
-	typedef std::tuple<int, int> Point;
+	typedef std::tuple<int, int> Room;
 
-	std::vector<std::string> paths(std::vector<std::string> acc);
+	class Dungeon {
+		char NORTH = 'N';
+		char SOUTH = 'S';
+		char EAST = 'E';
+		char WEST = 'W';
+		char BEGIN = '^';
+		char END = '$';
+		char OPEN = '(';
+		char CLOSE = ')';
+		char PIPE = '|';
+
+		std::map<Room, std::set<Room>> rooms;
+		Room current;
+		Room store;
+	public:
+		Dungeon();
+		void addRoom(Room number);
+		void move(char direction);
+		void restore();
+		void checkpoint();
+		int size();
+		std::set<Room> getRooms(Room room);
+	};
+
 	int Part1(std::string input);
 }
