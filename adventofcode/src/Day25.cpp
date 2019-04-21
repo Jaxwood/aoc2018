7b0ec34703415ea58ea736b3c1ad7c228039f7cf
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-04-21 20:09:49 +0200

    Add distance method

--- adventofcode/src/Day25.cpp
+++ adventofcode/src/Day25.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 namespace Day25 {
+	int distance(Point p1, Point p2) {
+		int a, b, c, d, e, f, g, h;
+		tie(a, b, c, d) = p1;
+		tie(e, f, g, h) = p2;
+		return abs(a - e) + abs(b - f) + abs(c - g) + abs(d - h);
+	}
 	int Part1(vector<Point> points) {
 		return 0;
 	}
